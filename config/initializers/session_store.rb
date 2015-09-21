7c636822b45768b3024716ae744fc0eaabaa2580
Samson Brock <SamsonLB@gmail.com>
2015-09-21 12:47:40 -0600

    Pivot project and rename to Walk This Way

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_bike_friendly_session'
+Rails.application.config.session_store :cookie_store, key: '_walk_this_way_session'
