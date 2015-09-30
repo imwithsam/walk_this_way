c00cb3d383cee062eaef57c120f58c3fb4f2f6c0
Samson Brock <SamsonLB@gmail.com>
2015-09-30 16:31:10 -0600

    Add low-level caching to Yelp API calls

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,27 +1,31 @@
 class YelpService
   def search_by_address(address)
-    response = Yelp.client.search(
-      address,
-      {
-        limit: 10
-      }
-    )
+    Rails.cache.fetch("yelp_search_by_address", expires_in: 15.minutes) do
+      response = Yelp.client.search(
+        address,
+        {
+          limit: 10
+        }
+      )
 
-    response.businesses.map do |business|
-      YelpBusiness.new(business)
+      response.businesses.map do |business|
+        YelpBusiness.new(business)
+      end
     end
   end
 
   def search_by_coordinates(coordinates)
-    response = Yelp.client.search_by_coordinates(
-      coordinates,
-      {
-        limit: 10
-      }
-    )
+    Rails.cache.fetch("yelp_search_by_coordinates", expires_in: 15.minutes) do
+      response = Yelp.client.search_by_coordinates(
+        coordinates,
+        {
+          limit: 10
+        }
+      )
 
-    response.businesses.map do |business|
-      YelpBusiness.new(business)
+      response.businesses.map do |business|
+        YelpBusiness.new(business)
+      end
     end
   end
 end
